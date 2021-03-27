0fbb2b8d601d4f32a1f8f3445aaea6c13052fa5b
juancruzalric94 <juancruzalric94@outlook.es>
2021-03-27 01:15:53 -0300

    primer commit de react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import data from './data'
+import {BrowserRouter, Link, Route} from "react-router-dom";
 import './App.css';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+
+  const openMenu = () => document.querySelector(".sidebar").classList.add("open");
+  const closeMenu = () => document.querySelector(".sidebar").classList.remove("open");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <Link to="/">Kioto</Link>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Carrito</a>
+                    <a href="signin.html">Empeza a disfrutar</a>
+
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Categorias</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Cuatris</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Motos</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <Route path="/products/:id" component={ProductScreen} />
+                    <Route path="/" exact={true} component={HomeScreen} />
+                </div>
+            </main>
+            <footer className="footer">
+                Todos los derechos reservados
+            </footer>
+        </div>
+        </BrowserRouter>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [
+    {
+        _id:'123',
+        name: "Uno",
+        cateogory: "Cars",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Volvo",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'124',
+        name: "Dos",
+        cateogory: "Van",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Audi",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'125',
+        name: "Uno",
+        cateogory: "Cars",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Volvo",
+        rating: 4.5,
+        numReviews: 10
+    },
+]
+}
