88cf2a0c9af17f9cc305c40b1195aae56e597024
juancruzalric94 <juancruzalric94@outlook.es>
2021-07-02 17:32:28 -0300

    imagenes en el about

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../juancruz.jpg';
+import logo2 from '../exequiel.jpg';
 
 function AboutScreen (props) {
 
@@ -8,7 +9,7 @@ function AboutScreen (props) {
         <h2 className="about-second-title">Ecommerce</h2>
         <div className="about-image">
             <img className="img-about" src={logo} width="200"/>
-            <img className="img-about" src={logo} width="200"/>
+            <img className="img-about" src={logo2} width="200"/>
         </div>
         <p>
             Nos especializamos en Compra de Motos, que esperas para realizar tu compra?
